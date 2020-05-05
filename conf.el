1f2c8259f795bc35cf43abb0651728753d4cc342
Jun Zhang <zhangjunphy@gmail.com>
2020-05-05 20:13:22 +0800

    update preamble and footer

--- conf.el
+++ conf.el
@@ -13,24 +13,21 @@
 ;;    ~/projects/blog/static/style.css
 ;;    and the favicon at
 ;;    ~/projects/blog/static/favicon.ico)
-(setq org-static-blog-page-header
-"<meta name=\"author\" content=\"John Dow\">
-<meta name=\"referrer\" content=\"no-referrer\">
-<link href= \"static/style.css\" rel=\"stylesheet\" type=\"text/css\" />
-<link rel=\"icon\" href=\"static/favicon.ico\">")
+;; (setq org-static-blog-page-header
+;; "<meta name=\"author\" content=\"John Dow\">
+;; <meta name=\"referrer\" content=\"no-referrer\">
+;; <link href= \"static/style.css\" rel=\"stylesheet\" type=\"text/css\" />
+;; <link rel=\"icon\" href=\"static/favicon.ico\">")
 
 ;; This preamble is inserted at the beginning of the <body> of every page:
 ;;   This particular HTML creates a <div> with a simple linked headline
-(setq org-static-blog-page-preamble
-"<div class=\"header\">
-  <a href=\"https://staticblog.org\">My Static Org Blog</a>
-</div>")
+;; (setq org-static-blog-page-preamble
+;; "<div class=\"header\">
+;;   <a href=\"https://staticblog.org\">My Static Org Blog</a>
+;; </div>")
 
 ;; This postamble is inserted at the end of the <body> of every page:
 ;;   This particular HTML creates a <div> with a link to the archive page
 ;;   and a licensing stub.
 (setq org-static-blog-page-postamble
-"<div id=\"archive\">
-  <a href=\"https://staticblog.org/archive.html\">Other posts</a>
-</div>
-<center><a rel=\"license\" href=\"https://creativecommons.org/licenses/by-sa/3.0/\"><img alt=\"Creative Commons License\" style=\"border-width:0\" src=\"https://i.creativecommons.org/l/by-sa/3.0/88x31.png\" /></a><br /><span xmlns:dct=\"https://purl.org/dc/terms/\" href=\"https://purl.org/dc/dcmitype/Text\" property=\"dct:title\" rel=\"dct:type\">bastibe.de</span> by <a xmlns:cc=\"https://creativecommons.org/ns#\" href=\"https://bastibe.de\" property=\"cc:attributionName\" rel=\"cc:attributionURL\">Bastian Bechtold</a> is licensed under a <a rel=\"license\" href=\"https://creativecommons.org/licenses/by-sa/3.0/\">Creative Commons Attribution-ShareAlike 3.0 Unported License</a>.</center>")
+"<center><a rel=\"license\" href=\"https://creativecommons.org/licenses/by-sa/3.0/\"><img alt=\"Creative Commons License\" style=\"border-width:0\" src=\"https://i.creativecommons.org/l/by-sa/3.0/88x31.png\" /></a><br /><span xmlns:dct=\"https://purl.org/dc/terms/\" href=\"https://purl.org/dc/dcmitype/Text\" property=\"dct:title\" rel=\"dct:type\">zhangjunphy.github.io</span> by <a xmlns:cc=\"https://creativecommons.org/ns#\" href=\"https://zhangjunphy.github.io\" property=\"cc:attributionName\" rel=\"cc:attributionURL\">zj</a> is licensed under a <a rel=\"license\" href=\"https://creativecommons.org/licenses/by-sa/3.0/\">Creative Commons Attribution-ShareAlike 3.0 Unported License</a>.</center>")
